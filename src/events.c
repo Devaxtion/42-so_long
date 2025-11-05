64c849502f280ec1018898f003f9b72a98f47a93
Leonardo Salgueiro <leonardo.j.r.salgueiro@gmail.com>
2025-11-05 22:06:27 +0000

    Some optimization in events

--- src/events.c
+++ src/events.c
@@ -12,77 +12,52 @@
 
 #include "so_long.h"
 
-static int	move_player(t_data *data, int keycode)
+static int	is_valid_move(t_data *data, char target)
 {
-	int	x_new;
-	int	y_new;
-
-	x_new = data->map.player.row;
-	y_new = data->map.player.col;
-	if (keycode == K_A)
-		x_new--;
-	if (keycode == K_D)
-		x_new++;
-	if (keycode == K_W)
-		y_new--;
-	if (keycode == K_S)
-		y_new++;
-
-	if (data->map.grid[y_new][x_new] == WALL)
+	if (target == WALL)
 		return (0);
-
-	if (data->map.grid[y_new][x_new] == COLLECTIBLE)
-	{
-		data->map.grid[data->map.player.col][data->map.player.row] = FLOOR;
-		data->map.grid[y_new][x_new] = PLAYER;
-		data->map.player.collected++;
-	}
-
-	if (data->map.grid[y_new][x_new] == EXIT)
-	{
-		if (data->map.player.collected < data->map.collectibles)
-			return (0);
-		data->map.grid[data->map.player.col][data->map.player.row] = FLOOR;
-		return (2);
-	}
-
-	// if 0, just move
-	if (data->map.grid[y_new][x_new] == FLOOR)
-	{
-		data->map.grid[data->map.player.col][data->map.player.row] = FLOOR;
-		data->map.grid[y_new][x_new] = PLAYER;
-	}
-
-	// Update player location
-	data->map.player.row = x_new;
-	data->map.player.col = y_new;
-
+	if (target == EXIT && data->map.player.collected < data->map.collectibles)
+		return (0);
 	return (1);
 }
 
-int	key_handler(int keycode, t_data *data)
+static void	update_player_pos(t_data *data, char target, int row_new, int col_new)
 {
-	int	code;
+	if (target == COLLECTIBLE)
+			data->map.player.collected++;
+	data->map.grid[data->map.player.col][data->map.player.row] = FLOOR;
+	data->map.grid[col_new][row_new] = PLAYER;
+	data->map.player.move_count += 1;
+	data->map.player.row = row_new;
+	data->map.player.col = col_new;
+}
 
-	code = 0;
-	// ft_printf("Keycode received: %d\n", keycode);
-	if (keycode == K_ESC)
-	{
-		ft_printf("Window closed by user. Exiting.\n");
-		cleanup_and_exit(0, NULL, data);
-	}
-	if (keycode == K_A || keycode == K_D || keycode == K_W || keycode == K_S)
-		code = move_player(data, keycode);
-	if (code == 2)
-	{
-		ft_printf("You won! Congratulations!\n");
-		cleanup_and_exit(0, NULL, data);
-	}
-	if (code == 1)
-	{
-		data->map.player.move_count += 1;
-		ft_printf("Player moved -> Count = %i\n", data->map.player.move_count);
-	}
+static int	move_player(t_data *data, int keycode)
+{
+	int		row_new;
+	int		col_new;
+	char	target;
+
+	row_new = data->map.player.row;
+	col_new = data->map.player.col;
+	if (keycode == K_A)
+		row_new--;
+	else if (keycode == K_D)
+		row_new++;
+	else if (keycode == K_W)
+		col_new--;
+	else if (keycode == K_S)
+		col_new++;
+	else
+		return (0);
+	target = data->map.grid[col_new][row_new];
+	if (!is_valid_move(data, target))
+		return (0);
+	update_player_pos(data, target, row_new, col_new);
+	if (target == EXIT)
+		return (2);
+	if (target == FLOOR || target == COLLECTIBLE)
+		return (1);
 	return (0);
 }
 
@@ -90,5 +65,22 @@ int	destroy_handler(t_data *data)
 {
 	ft_printf("Window closed by user. Exiting.\n");
 	cleanup_and_exit(0, NULL, data);
+	return (0);
+}
+
+int	key_handler(int keycode, t_data *data)
+{
+	int	code;
+
+	if (keycode == K_ESC)
+		destroy_handler(data);
+	code = move_player(data, keycode);
+	if (code == 1)
+		ft_printf("Player moved -> Count = %i\n", data->map.player.move_count);
+	else if (code == 2)
+	{
+		ft_printf("You won with %i moves!\n", data->map.player.move_count);
+		cleanup_and_exit(0, NULL, data);
+	}
 	return (0);
 }
